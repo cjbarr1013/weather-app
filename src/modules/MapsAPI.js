3fbe2302a6f3a2d633e0341f620aa36df0784315
Cameron Barr <cjbarr1013@gmail.com>
2024-11-23 11:11:52 -0500

    Create interface and styling for JS manipulation

---
+++ src/modules/MapsAPI.js
@@ -0,0 +1,27 @@
+export function MapsAPI() {
+  const apiKey = 'AIzaSyAWggmxnep1wqmo8PlO0B8bKURkECp3D9U';
+  const zoom = '14';
+
+  const getUrl = (coordinates) => {
+    return (
+      'https://www.google.com/maps/embed/v1/view?key=' +
+      apiKey +
+      '&center=' +
+      coordinates +
+      '&zoom=' +
+      zoom
+    );
+  };
+
+  const getMap = (coordinates) => {
+    const mapFrame = document.createElement('iframe');
+    mapFrame.referrerPolicy = 'no-referrer-when-downgrade';
+    mapFrame.src = getUrl(coordinates);
+
+    return mapFrame;
+  };
+
+  return {
+    getMap,
+  };
+}
